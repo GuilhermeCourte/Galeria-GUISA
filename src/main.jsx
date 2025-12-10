c226ba2f8b98e8a6396797361c2c6f82c57b2b8d
GuilhermeCourte <guicourte0@gmail.com>
2025-12-10 19:41:39 -0300

    wwwadwawdawdawdwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css' // Importa o CSS que definimos anteriormente
 
+// Bloqueia o botão direito do mouse
+document.addEventListener('contextmenu', event => event.preventDefault());
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
